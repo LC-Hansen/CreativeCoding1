d3abe32a060b5c1ba57497843ad59021831a0d16
Landon Hansen <54595571+LC-Hansen@users.noreply.github.com>
2019-10-25 12:40:45 -0600

    cc1homework-09

---
+++ cc1homework-09/sketch.js
@@ -0,0 +1,38 @@
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(223, 243, 255);
+  fill (250, 240, 230);
+  ellipse (195,120,150,200);
+  fill (255,255,255);
+  ellipse (155,140,38,32);
+  fill (255,255,255);
+  ellipse (235,140,38,32);
+  fill (223, 243, 255);
+  circle (155,140,30);
+  fill (223, 243, 255);
+  circle (235,140,30);
+  line (195,200,170,200);
+  fill (0,0,0);
+  circle (155,140,10);
+  fill (0,0,0);
+  circle (235,140,10);
+  fill (255, 255, 224);
+  rect (130,20,130,30);
+  rect (120,40,120,30);
+  rect (250,55,15,40);
+  triangle (120,40,120,25,135,40);
+  triangle (135,40,138,25,145,40);
+  triangle (130,20,130,15,135,40);
+  fill (255,255,255);
+  point (157,137);
+  fill (148, 0, 211);
+  textSize (15);
+  text ("Title: I hope my head isn't truly shaped like that", 45, 245);
+  text ("Or: But that haircut though", 100, 260);
+  textSize (12);
+  text ("By: Landon Hansen", 260,300);
+
+}
